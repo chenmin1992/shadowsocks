938662ff7d4e563afbcedbcfdb8c9f553a84cc9d
chenmin <781022537@qq.com>
2018-02-02 23:08:15 +0800

    fix cp config file

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd `dirname $0`
-chmod +x *.sh
 cp apiconfig.py userapiconfig.py
-cp config.json user_config.json
-cp shadowsocks.ini /etc/supervisord.d/shadowsocks.ini
+cp config.json user-config.json
+chmod +x *.sh
